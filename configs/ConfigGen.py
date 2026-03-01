18158433150566e422b1af08c388c27578534547
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2026-03-01 13:52:34 -0500

    Update ConfigGen.py

--- configs/ConfigGen.py
+++ configs/ConfigGen.py
@@ -318,7 +318,7 @@ def main() -> int:
         return 1
 
     if not os.path.isfile(args.file):
-        print('ERROR: file "{args.file}" does not exist')
+        pprint('ERROR: file "' + args.file + '" does not exist')
         return 1
 
     data = ParseConfigFile(args.file)
